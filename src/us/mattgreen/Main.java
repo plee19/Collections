a93f0b79a7aba130f3e44864e6e4dcf8f60e4138
plee19 <plee19@my.wctc.edu>
2018-09-12 21:27:09 -0500

    Initial commit

---
+++ src/us/mattgreen/Main.java
@@ -0,0 +1,44 @@
+package us.mattgreen;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+//LIST TOP TEN MOST USED WORDS
+    private final static FileInput indata = new FileInput("the_book.csv");
+    private final static Map<String, Integer> map = new HashMap<String, Integer>();
+
+    public static void main(String[] args) {
+        String line;
+        String[] words;
+        Object wordFound;
+     //   String[] fields;
+
+        while ((line = indata.fileReadLine()) != null) {
+            line=line.replace(",","").replace(".","")
+                    .replace(";","").replace(":","")
+                    .replace("'","").replace("\"","")
+                    .replace("-","").replace("!","")
+                    .replace("#","").replace("(","")
+                    .replace(")","").replace("?","")
+                    .replace("_"," ").replace("?","")
+                    .toLowerCase().trim();
+            words = line.split(" ");
+            for (String s:words) {
+                wordFound = map.get(s);
+                if (wordFound == null) {
+                    map.put(s, new Integer(1));
+                }
+                else {
+                    map.put(s, map.get(s) + 1);
+                }
+
+            }
+
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                System.out.println(entry.getKey() + " " + entry.getValue());
+            }
+        }
+    }
+    
+}
